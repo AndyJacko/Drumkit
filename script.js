a1e42b969e82fc0a1fe98ef933b2dc4dbf675213
Andy Jacko <wisdom.mcr@gmail.com>
2022-09-13 12:30:52 +0100

    Completed responsive design...

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const ride = document.getElementById("ride");
 const snare = document.getElementById("snare");
 const tink = document.getElementById("tink");
 const tom = document.getElementById("tom");
+const tss = document.getElementById("badumtss");
+const intro = document.getElementById("intro");
+const egg = document.getElementById("easteregg");
 
 const canvas = document.getElementById("playground");
 const ctx = canvas.getContext("2d");
@@ -169,14 +172,94 @@ document.addEventListener("keydown", (e) => {
       drawShape();
       break;
     case "l":
-      boom.pause();
-      boom.currentTime = 0;
+      tom.pause();
+      tom.currentTime = 0;
       tom.play();
       drawShape();
       break;
     case "Escape":
+      tss.pause();
+      tss.currentTime = 0;
+      tss.play();
       ctx.fillStyle = "white";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       break;
   }
 });
+
+const playSound = (key) => {
+  switch (key) {
+    case "a":
+      boom.pause();
+      boom.currentTime = 0;
+      boom.play();
+      drawShape();
+      break;
+    case "s":
+      clap.pause();
+      clap.currentTime = 0;
+      clap.play();
+      drawShape();
+      break;
+    case "d":
+      hihat.pause();
+      hihat.currentTime = 0;
+      hihat.play();
+      drawShape();
+      break;
+    case "f":
+      kick.pause();
+      kick.currentTime = 0;
+      kick.play();
+      drawShape();
+      break;
+    case "g":
+      openhat.pause();
+      openhat.currentTime = 0;
+      openhat.play();
+      drawShape();
+      break;
+    case "h":
+      ride.pause();
+      ride.currentTime = 0;
+      ride.play();
+      drawShape();
+      break;
+    case "j":
+      snare.pause();
+      snare.currentTime = 0;
+      snare.play();
+      drawShape();
+      break;
+    case "k":
+      tink.pause();
+      tink.currentTime = 0;
+      tink.play();
+      drawShape();
+      break;
+    case "l":
+      tom.pause();
+      tom.currentTime = 0;
+      tom.play();
+      drawShape();
+      break;
+  }
+};
+
+const playIntro = () => {
+  intro.pause();
+  intro.currentTime = 1;
+  intro.play();
+};
+
+const badumtss = () => {
+  tss.pause();
+  tss.currentTime = 0;
+  tss.play();
+};
+
+const easteregg = () => {
+  egg.pause();
+  egg.currentTime = 0;
+  egg.play();
+};
